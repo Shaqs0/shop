4dfa9fea15e9a4a244c4d3545ddf4cfcbc94a247
Shaqs0 <hhgfhhhggb@gmail.com>
2024-05-04 11:21:44 +0300

    update slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -3,18 +3,21 @@ import '@splidejs/splide/css/skyblue';
 import styles from './Slider.module.css';
 
 export function Slider() {
-	
 
 	return (
 		<div className={styles['slider']}>
 			<Splide aria-label="My Favorite Images">
-				<SplideSlide>
-					<img className={styles['slider_img']} src="src/components/Slider/image1.jpg" alt="Image 1"/>
+				<SplideSlide className={styles['slide']}>
+					<img className={styles['slider_img']}
+						src="src/components/Slider/images/big-1920.jpg"
+						alt='Big'
+						srcSet='src/components/Slider/images/big-500.jpg 500w, src/components/Slider/images/big-900.jpg 900w, src/components/Slider/images/big-1000.jpg 3000w,'
+					/>
 				</SplideSlide>
-				<SplideSlide>
+				<SplideSlide className={styles['slide']}>
 					<img className={styles['slider_img']} src="src/components/Slider/image2.jpg" alt="Image 2"/>
 				</SplideSlide>
-				<SplideSlide>
+				<SplideSlide className={styles['slide']}>
 					<img className={styles['slider_img']} src="src/components/Slider/image1.jpg" alt="Image 1"/>
 				</SplideSlide>
 			</Splide>
